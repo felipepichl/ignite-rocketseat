275ee329834c7c11a5429fc851039f0b2b8d605f
Felipe Pichl <felipe.pichl@hotmail.com>
2021-12-10 00:22:42 -0300

    Node-JS_API: M02 - Class 01

--- node-js/src/start/server.ts
+++ node-js/src/start/server.ts
@@ -1,9 +1,7 @@
-import express from 'express';
-
-import { createCourse } from './routes'
+import express from "express";
 
 const app = express();
 
-app.get('/', createCourse);
-
-app.listen(3333);
+app.listen(3333, () => {
+  console.log("Server running in port 3333");
+});
