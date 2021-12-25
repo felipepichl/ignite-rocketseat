f3934fc5d53cf4d5f366c4d8ba80adbb6a8dc2c6
Felipe Pichl <felipe.pichl@hotmail.com>
2021-12-25 16:52:09 -0300

    Node-JS_API: M04 - Class 04

--- node-js/src/routes/categories.routes.ts
+++ node-js/src/routes/categories.routes.ts
@@ -1,23 +1,11 @@
 import { Router } from "express";
 
 import { CategoryRepository } from "../modules/cars/repositories/CategoryRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
-const categoryRepository = new CategoryRepository();
 
-categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const service = new CreateCategoryService(categoryRepository);
-
-  service.execute({
-    name,
-    description,
-  });
-
-  return response.status(201).send();
-});
+categoriesRoutes.post("/", createCategoryController.handle);
 
 categoriesRoutes.get("/", (request, response) => {
   const repository = new CategoryRepository();
