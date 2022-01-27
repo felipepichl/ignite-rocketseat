f043b1a799e0656820dc314f81eeed5a46bdcbbe
Felipe Pichl <felipe.pichl@hotmail.com>
2022-01-26 21:17:14 -0300

    Node-JS_API: CIII, M04 - Class 04

---
+++ node-js/src/database/migrations/1643242238403-AlterUserDeleteUsername.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export default class AlterUserDeleteUsername1643242238403
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("users", "username");
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      "users",
+      new TableColumn({
+        name: "username",
+        type: "varchar",
+        isUnique: true,
+      })
+    );
+  }
+}
