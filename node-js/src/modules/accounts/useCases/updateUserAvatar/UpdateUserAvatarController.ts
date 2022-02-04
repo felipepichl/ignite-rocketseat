f1a1c05ddb018b98e1f14708608f2b6bcd89374b
Felipe Pichl <felipe.pichl@hotmail.com>
2022-02-04 00:49:52 -0300

    Node-JS_API: CIII, M05 - Class 01, Me: request

--- node-js/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ node-js/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -5,11 +5,14 @@ import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
 
 class UpdateUserAvatarController {
   async handle(request: Request, response: Response): Promise<Response> {
+    const { id: user_id } = request.user;
+    const { filename: avatar_file } = request.file;
+
     const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
 
     await updateUserAvatarUseCase.execute({
-      user_id: request.user.id,
-      avatar_file: request.file.filename,
+      user_id,
+      avatar_file,
     });
 
     return response.status(200).send();
