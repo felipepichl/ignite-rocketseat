d368c298d390c51af9f0cb9a5a2b304f632a5271
Felipe Pichl <felipe.pichl@hotmail.com>
2022-02-05 08:02:58 -0300

    Node-JS_API: CIII, M05 - Class 02 - Part01

--- node-js/src/routes/users.routes.ts
+++ node-js/src/routes/users.routes.ts
@@ -1,15 +1,13 @@
 import { Router } from "express";
 import multer from "multer";
 
+import uploadConfig from "../config/upload";
 import { enshureAuthenticated } from "../middlewares/enshureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
-
-const upload = multer({
-  dest: "./avatar",
-});
+const upload = multer(uploadConfig.upload("./tmp/avatar"));
 
 const createUsersController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
