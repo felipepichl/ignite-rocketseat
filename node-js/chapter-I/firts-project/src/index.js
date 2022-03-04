c18ec146d821691926303ce724caab203f562a26
Felipe Pichl <felipe.pichl@hotmail.com>
2022-03-04 13:46:49 -0300

    Node-JS_API: CI, M03 - Class 02

--- node-js/chapter-I/firts-project/src/index.js
+++ node-js/chapter-I/firts-project/src/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 app.use(express.json());
 
+const customers = [];
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+
+  const id = uuid();
+
+  customers.push({
+    id,
+    name,
+    cpf,
+    statment: []
+  });
+
+  return response.status(201).send();
+
+});
 app.listen(3333);
