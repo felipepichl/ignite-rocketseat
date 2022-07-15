d77264218b25f24efec58ad5facdaf91f5bb6871
Felipe Pichl <felipe.pichl@hotmail.com>
2022-07-15 17:22:25 -0300

    Node-JS_API: Fix ManyToMany SQLite

---
+++ node-js/project/src/shared/infra/typeorm/migrations/1657915754001-FixCreateSpecificationsCars.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class FixCreateSpecificationsCars1657915754001
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn(
+      "specifications_cars",
+      new TableColumn({
+        name: "created_at",
+        type: "timestamp",
+        default: "now()",
+      })
+    );
+
+    await queryRunner.addColumn(
+      "specifications_cars",
+      new TableColumn({
+        name: "created_at",
+        type: "timestamp",
+        default: "datetime('now')",
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("created_at", "specifications_cars");
+    await queryRunner.addColumn(
+      "specifications_cars",
+      new TableColumn({
+        name: "created_at",
+        type: "timestamp",
+        default: "now()",
+      })
+    );
+  }
+}
