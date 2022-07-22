b08cd411f35481eac4b3f06408c17d37429bcd71
Felipe Pichl <felipe.pichl@hotmail.com>
2022-07-21 23:27:16 -0300

    Node-JS_API: CIV, M03 - Class 15 Done

--- node-js/project/src/shared/infra/http/routes/cars.routes.ts
+++ node-js/project/src/shared/infra/http/routes/cars.routes.ts
@@ -1,18 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "@config/upload";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarController";
+import { UploadCarImageController } from "@modules/cars/useCases/uploadCarImages/UploadCarImageController";
 
 import { enshureAdmin } from "../middlewares/enshureAdmin";
 import { enshureAuthenticated } from "../middlewares/enshureAuthenticated";
 
 const carsRoutes = Router();
+const upload = multer(uploadConfig.upload("./tmp/cars"));
 
 const createCarController = new CreateCarController();
 const createCarSpeficificationController =
   new CreateCarSpecificationController();
 const listAvailableCarController = new ListAvailableCarController();
+const uploadCarImagesController = new UploadCarImageController();
 
 carsRoutes.post(
   "/",
@@ -29,5 +34,13 @@ carsRoutes.post(
 );
 
 carsRoutes.get("/available", listAvailableCarController.handle);
+
+carsRoutes.post(
+  "/images/:id",
+  enshureAuthenticated,
+  enshureAdmin,
+  upload.array("images"),
+  uploadCarImagesController.handle
+);
 
 export { carsRoutes };
