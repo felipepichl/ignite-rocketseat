beff5600ba4733ec54c6a203f260f7ad57e2ba1d
Felipe Pichl <felipe.pichl@hotmail.com>
2022-07-28 19:24:33 -0300

    Node-JS_API: CIV, M05 - Class 02 - Part I

--- node-js/project/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ node-js/project/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -2,6 +2,13 @@ import request from "supertest";
 
 import { app } from "@shared/infra/http/start/app";
 
-describe("Create Category Controller", async () => {
-  await request(app).get("/cars/available").expect(200);
+describe("Create Category Controller", () => {
+  it("should be able to create a new category", async () => {
+    const response = await request(app).post("/categories").send({
+      name: "Category supertest",
+      description: "Description category supertest",
+    });
+
+    expect(response.status).toBe(201);
+  });
 });
