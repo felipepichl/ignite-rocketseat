75e0061734eb419edc98a8e326b5208e9e08cce3
Felipe Pichl <felipe.pichl@hotmail.com>
2022-07-28 19:41:34 -0300

    Node-JS_API: CIV, M05 - Class 02 - Part II

--- node-js/project/src/shared/infra/typeorm/index.ts
+++ node-js/project/src/shared/infra/typeorm/index.ts
@@ -1,8 +1,13 @@
 import { DataSource } from "typeorm";
 
+const database = {
+  dev: "./src/shared/infra/typeorm/database.sqlite",
+  test: "./src/shared/infra/typeorm/database_test.sqlite",
+};
+
 const AppDataSource = new DataSource({
   type: "sqlite",
-  database: "./src/shared/infra/typeorm/database.sqlite",
+  database: process.env.NODE_ENV === "test" ? database.test : database.dev,
   entities: ["./src/modules/**/infra/typeorm/entities/*.ts"],
   migrations: ["./src/shared/infra/typeorm/migrations/*.ts"],
 });
