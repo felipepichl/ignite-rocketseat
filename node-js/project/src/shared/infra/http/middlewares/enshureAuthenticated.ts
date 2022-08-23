ff4d3f5020c3707dac355df09914085f138658ce
Felipe Pichl <felipe.pichl@hotmail.com>
2022-08-22 23:27:10 -0300

    Node-JS_API: CV, M02 - Class 06

--- node-js/project/src/shared/infra/http/middlewares/enshureAuthenticated.ts
+++ node-js/project/src/shared/infra/http/middlewares/enshureAuthenticated.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 
 import { authConfig } from "@config/auth";
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface ITokenPayload {
@@ -16,6 +16,8 @@ export async function enshureAuthenticated(
 ): Promise<void> {
   const authHeader = request.headers.authorization;
 
+  const userTokenRepository = new UsersTokensRepository();
+
   if (!authHeader) {
     throw new AppError("Token is missing", 401);
   }
@@ -23,14 +25,16 @@ export async function enshureAuthenticated(
   const [, token] = authHeader.split(" ");
 
   try {
-    const { secret } = authConfig;
+    const { secret_refresh_token } = authConfig;
 
-    const decoded = verify(token, secret);
+    const decoded = verify(token, secret_refresh_token);
 
     const { sub: user_id } = decoded as ITokenPayload;
 
-    const usersRepository = new UsersRepository();
-    const user = await usersRepository.findById(user_id);
+    const user = await userTokenRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token
+    );
 
     if (!user) {
       throw new AppError("User does not exists", 401);
