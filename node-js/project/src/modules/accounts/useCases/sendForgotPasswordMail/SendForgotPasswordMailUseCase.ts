5b134534ff76b23f19313f1c94eaa058d354524c
Felipe Pichl <felipe.pichl@hotmail.com>
2022-08-23 23:34:52 -0300

    Node-JS_API: CV, M03 - Class 01 - Part II

--- node-js/project/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ node-js/project/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,6 +1,8 @@
 import { injectable, inject } from "tsyringe";
+import { v4 as uuid } from "uuid";
 
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
@@ -11,15 +13,25 @@ interface IRequest {
 class SendForgotPasswordMailUseCase {
   constructor(
     @inject("UsersRepository")
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject("UsersTokenRepository")
+    private usersTokenRepository: IUsersTokensRepository
   ) {}
 
   async execute({ email }: IRequest): Promise<void> {
-    const user = this.usersRepository.findByEmail(email);
+    const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
       throw new AppError("User does not exists");
     }
+
+    const token = uuid();
+
+    await this.usersTokenRepository.create({
+      user_id: user.id,
+      refresh_token: token,
+    });
   }
 }
 
