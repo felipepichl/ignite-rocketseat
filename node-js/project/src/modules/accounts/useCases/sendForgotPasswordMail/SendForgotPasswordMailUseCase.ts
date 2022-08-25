6378d15f64c825067c71644c1f30fcb0b84cad62
Felipe Pichl <felipe.pichl@hotmail.com>
2022-08-24 23:29:39 -0300

    Node-JS_API: CV, M03 - Class 01 - Part III

--- node-js/project/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ node-js/project/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -3,6 +3,7 @@ import { v4 as uuid } from "uuid";
 
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/model/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
@@ -14,9 +15,10 @@ class SendForgotPasswordMailUseCase {
   constructor(
     @inject("UsersRepository")
     private usersRepository: IUsersRepository,
-
     @inject("UsersTokenRepository")
-    private usersTokenRepository: IUsersTokensRepository
+    private usersTokenRepository: IUsersTokensRepository,
+    @inject("DateProvider")
+    private dateProvider: IDateProvider
   ) {}
 
   async execute({ email }: IRequest): Promise<void> {
@@ -28,9 +30,12 @@ class SendForgotPasswordMailUseCase {
 
     const token = uuid();
 
+    const expires_date = this.dateProvider.addHours(3);
+
     await this.usersTokenRepository.create({
       user_id: user.id,
       refresh_token: token,
+      expires_date,
     });
   }
 }
