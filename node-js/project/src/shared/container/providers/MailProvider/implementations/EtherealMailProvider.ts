3155593120daaf45750da8eb0813f787d86733ce
Felipe Pichl <felipe.pichl@hotmail.com>
2022-08-25 23:35:57 -0300

    Node-JS_API: CV, M03 - Class 02 - Part II

--- node-js/project/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ node-js/project/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -6,23 +6,34 @@ class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
 
   constructor() {
-    nodemailer.createTestAccount().then((account) => {
-      const transporter = nodemailer.createTransport({
-        host: account.smtp.host,
-        port: account.smtp.port,
-        secure: account.smtp.secure,
-        auth: {
-          user: account.user,
-          pass: account.pass,
-        },
-      });
+    nodemailer
+      .createTestAccount()
+      .then((account) => {
+        const transporter = nodemailer.createTransport({
+          host: account.smtp.host,
+          port: account.smtp.port,
+          secure: account.smtp.secure,
+          auth: {
+            user: account.user,
+            pass: account.pass,
+          },
+        });
 
-      this.client = transporter;
-    });
+        this.client = transporter;
+      })
+      .catch((err) => console.error(err));
   }
 
   async sendMail(to: string, subject: string, body: string): Promise<void> {
-    throw new Error("Method not implemented.");
+    const message = await this.client.sendMail({
+      from: "Rentalx <noreplay@rentalx.com>",
+      to,
+      subject,
+      text: body,
+    });
+
+    console.log("Message send: %s", message.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(message));
   }
 }
 
