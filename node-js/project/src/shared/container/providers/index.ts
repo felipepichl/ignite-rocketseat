ef77832751723368ffa4873c54af2bcb8947eb44
Felipe Pichl <felipe.pichl@hotmail.com>
2022-08-25 23:26:23 -0300

    Node-JS_API: CV, M03 - Class 02 - Part I

--- node-js/project/src/shared/container/providers/index.ts
+++ node-js/project/src/shared/container/providers/index.ts
@@ -2,9 +2,17 @@ import { container } from "tsyringe";
 
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
 import { IDateProvider } from "./DateProvider/model/IDateProvider";
+import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
+import { IMailProvider } from "./MailProvider/model/IMailProvider";
 
 const providers = {
   dayjs: DayjsDateProvider,
+  etherealMail: EtherealMailProvider,
 };
 
 container.registerSingleton<IDateProvider>("DateProvider", providers.dayjs);
+
+container.registerSingleton<IMailProvider>(
+  "MailProvider",
+  providers.etherealMail
+);
