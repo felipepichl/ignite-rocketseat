edd47994b68e0be9d98707effdd464cebe028a53
Felipe Pichl <felipe.pichl@hotmail.com>
2022-09-14 23:10:55 -0300

    Prisma-IO: CI, M02 - Class 01 - aula06 Part I

---
+++ node-js/prisma-io/class-06/server.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const result = await prisma.courses.create({
+    data: {
+      duration: 190,
+      name: 'NodeJS Course',
+      description: 'Great NodeJS course'
+    }
+  });
+}
+
+main();
