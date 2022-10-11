6f6bfe08059889dc4b00d432f70b9450a78a2605
Felipe Pichl <felipe.pichl@hotmail.com>
2022-10-10 23:40:10 -0300

    Node-JS: Prisma UsersTokensRepository

---
+++ node-js/project/src/modules/accounts/infra/prisma/repositories/UsersTokensRepository.ts
@@ -0,0 +1,52 @@
+import { ICreateUserTokensDTO } from "@modules/accounts/dtos/ICreateUserTokensDTO";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { PrismaClient } from "@prisma/client";
+
+import { UserTokens } from "../models/UserTokens";
+
+class UsersTokensRepository implements IUsersTokensRepository {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  async create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUserTokensDTO): Promise<UserTokens> {
+    const result = await this.prisma.userTokens.create({
+      data: {
+        expires_date,
+        refresh_token,
+        fk_user_id: user_id,
+      },
+    });
+
+    return result;
+  }
+
+  async findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string
+  ): Promise<UserTokens> {
+    const result = await this.prisma.userTokens.findFirst({
+      where: {
+        fk_user_id: user_id,
+        refresh_token,
+      },
+    });
+
+    return result;
+  }
+
+  async deleteById(id: string): Promise<void> {
+    await this.prisma.userTokens.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
+export { UsersTokensRepository };
