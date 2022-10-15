acc4b3341ce663fe2fe27d6785f6f22d0e74bf02
Felipe Pichl <felipe.pichl@hotmail.com>
2022-10-15 14:16:07 -0300

    Node-JS: Prisma RentalRepository

---
+++ node-js/project/src/modules/rentals/infra/prisma/models/Rental.ts
@@ -0,0 +1,23 @@
+import { v4 as uuid } from "uuid";
+
+import { Rental as IRental } from "@prisma/client";
+
+class Rental implements IRental {
+  id: string;
+  start_date: Date;
+  end_date: Date;
+  expected_return_date: Date;
+  total: number;
+  created_at: Date;
+  updated_at: Date;
+  fk_car_id: string;
+  fk_user_id: string;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Rental };
---
+++ node-js/project/src/modules/rentals/infra/prisma/repositories/RentalsRepository.ts
@@ -0,0 +1,68 @@
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { PrismaClient } from "@prisma/client";
+
+import { Rental } from "../models/Rental";
+
+class RentalsRepository implements IRentalsRepository {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  async create({
+    id,
+    car_id,
+    user_id,
+    expected_return_date,
+    end_date,
+    total,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const result = await this.prisma.rental.create({
+      data: {
+        id,
+        fk_car_id: car_id,
+        fk_user_id: user_id,
+        expected_return_date,
+        end_date,
+        total,
+      },
+    });
+
+    return result;
+  }
+
+  async findOpenRentalByCar(car_id: string): Promise<Rental> {
+    const result = await this.prisma.rental.findFirst({
+      where: { fk_car_id: car_id, end_date: null },
+    });
+
+    return result;
+  }
+  async findOpenRentalByUser(user_id: string): Promise<Rental> {
+    const result = await this.prisma.rental.findFirst({
+      where: { fk_user_id: user_id, end_date: null },
+    });
+
+    return result;
+  }
+
+  async findById(id: string): Promise<Rental> {
+    const result = await this.prisma.rental.findUnique({
+      where: { id },
+    });
+
+    return result;
+  }
+
+  async findByUser(user_id: string): Promise<Rental[]> {
+    const result = await this.prisma.rental.findMany({
+      where: { fk_user_id: user_id },
+    });
+
+    return result;
+  }
+}
+
+export { RentalsRepository };
