08ae9cfbab093358f0b1d52c35146218476b9113
Felipe Pichl <felipe.pichl@hotmail.com>
2022-11-13 00:47:00 -0300

    Node-JS: Refactoring rental - Class 05 - Me

--- node-js/project/src/shared/infra/http/routes/rental.routes.ts
+++ node-js/project/src/shared/infra/http/routes/rental.routes.ts
@@ -1,27 +1,27 @@
 import { Router } from "express";
 
-import { CreateRentalController } from "@modules/rentals/useCases/createRentals/CreateRentalController";
-import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
-import { ListRentalByUserController } from "@modules/rentals/useCases/listRentalByUser/ListRentalByUserController";
+import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+// import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+// import { ListRentalByUserController } from "@modules/rentals/useCases/listRentalByUser/ListRentalByUserController";
 
 import { enshureAuthenticated } from "../middlewares/enshureAuthenticated";
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
-const devolutionRentalController = new DevolutionRentalController();
-const listRentalByUserController = new ListRentalByUserController();
+// const devolutionRentalController = new DevolutionRentalController();
+// const listRentalByUserController = new ListRentalByUserController();
 
 rentalRoutes.post("/", enshureAuthenticated, createRentalController.handle);
-rentalRoutes.post(
-  "/devolution/:id",
-  enshureAuthenticated,
-  devolutionRentalController.handle
-);
-rentalRoutes.get(
-  "/user",
-  enshureAuthenticated,
-  listRentalByUserController.handle
-);
+// rentalRoutes.post(
+//   "/devolution/:id",
+//   enshureAuthenticated,
+//   devolutionRentalController.handle
+// );
+// rentalRoutes.get(
+//   "/user",
+//   enshureAuthenticated,
+//   listRentalByUserController.handle
+// );
 
 export { rentalRoutes };
