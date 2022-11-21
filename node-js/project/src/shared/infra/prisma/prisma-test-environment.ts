9b34ba9346e3e7b28849f8a45b027a7ed498cdec
Felipe Pichl <felipe.pichl@hotmail.com>
2022-11-21 07:38:12 -0300

    Node-JS: Doing E2E Prisma, Part I

--- node-js/project/src/shared/infra/prisma/prisma-test-environment.ts
+++ node-js/project/src/shared/infra/prisma/prisma-test-environment.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import NodeEnvironment from "jest-environment-node";
 import { exec } from "node:child_process";
-import crypto from "node:crypto";
+// import crypto from "node:crypto";
 import util from "node:util";
 
 import type { Config } from "@jest/types";
@@ -13,20 +13,14 @@ const execSync = util.promisify(exec);
 const prismaBinary = "./node_modules/.bin/prisma";
 
 export default class PrismaTestEnvironment extends NodeEnvironment {
-  private schema: string;
+  // private schema: string;
   private connectionString: string;
 
   constructor(config: Config.ProjectConfig) {
     super(config);
 
-    const dbUser = process.env.DATABASE_USER;
-    const dbPass = process.env.DATABASE_PASS;
-    const dbHost = process.env.DATABASE_HOST;
-    const dbPort = process.env.DATABASE_PORT;
-    const dbName = process.env.DATABASE_NAME;
-
-    this.schema = `test_${crypto.randomUUID()}`;
-    this.connectionString = `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?schema=${this.schema}`;
+    // this.schema = `test_${crypto.randomUUID()}`;
+    this.connectionString = process.env.DATABASE_URL;
   }
 
   async setup() {
@@ -39,12 +33,11 @@ export default class PrismaTestEnvironment extends NodeEnvironment {
   }
 
   async teardown() {
-    const client = new Client({
-      connectionString: this.connectionString,
-    });
-
-    await client.connect();
-    await client.query(`DROP SCHEMA IF EXISTS "${this.schema}" CASCADE`);
-    await client.end();
+    // const client = new Client({
+    //   connectionString: this.connectionString,
+    // });
+    // await client.connect();
+    // await client.query(`DROP SCHEMA IF EXISTS "${this.schema}" CASCADE`);
+    // await client.end();
   }
 }
