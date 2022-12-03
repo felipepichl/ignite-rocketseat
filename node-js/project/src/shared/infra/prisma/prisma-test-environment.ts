85fd6d07d757b1fb12d739f811d7394bc81381e3
Felipe Pichl <felipe.pichl@hotmail.com>
2022-12-02 21:04:47 -0300

    Node-JS: Prisma Environment

--- node-js/project/src/shared/infra/prisma/prisma-test-environment.ts
+++ node-js/project/src/shared/infra/prisma/prisma-test-environment.ts
@@ -6,8 +6,6 @@ import crypto from 'node:crypto'
 
 import type { Config } from '@jest/types';
 
-//import type { EnvironmentContext, JestEnvironmentConfig } from '@jest/environment';
-
 dotenv.config({ path: '.env.testing' });
 
 const execSync = util.promisify(exec);
@@ -17,29 +15,27 @@ const prismaBinary = './node_modules/.bin/prisma';
 export default class PrismaTestEnvironment extends NodeEnvironment {
   private schema: string;
   private connectionString: string;
+  private prismaLocation: string;
+
 
   constructor(config: Config.ProjectConfig) {
     super(config);
 
     this.schema = `test_${crypto.randomUUID()}.db`;
-    this.connectionString = `file:./${this.schema}`
+    this.connectionString = `file:./${this.schema}`;
+    this.prismaLocation = '--schema=./src/shared/infra/prisma/schema.prisma'
   }
 
   async setup() {
     process.env.DATABASE_URL = this.connectionString;
     this.global.process.env.DATABASE_URL = this.connectionString;
 
-    await execSync(`${prismaBinary} migrate up --create-db --experimental`)
+    await execSync(`${prismaBinary} migrate deploy ${this.prismaLocation}`)
 
     return super.setup();
   }
 
   async teardown() {
-    // const client = new Client({
-    //   connectionString: this.connectionString,
-    // });
-    // await client.connect();
-    // await client.query(`DROP SCHEMA IF EXISTS "${this.schema}" CASCADE`);
-    // await client.end();
+    //ToDo
   }
 }
