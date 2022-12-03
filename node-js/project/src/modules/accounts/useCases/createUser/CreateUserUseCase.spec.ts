16c8689a327194430b39cd08f4d6f81648173b59
Felipe Pichl <felipe.pichl@hotmail.com>
2022-12-02 21:14:26 -0300

    Node-JS: Doing users tests

---
+++ node-js/project/src/modules/accounts/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,27 @@
+
+import { UserRepositoryInMemory } from '../../repositories/in-memory/UserRepositoryInMemory'
+
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+let createUserUseCase: CreateUserUseCase;
+let usersRepositoryInMemory: UserRepositoryInMemory;
+
+describe('Create Users', () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UserRepositoryInMemory();
+
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+
+  it('should be able to create a new user', async () => {
+    const user = await createUserUseCase.execute({
+      name: 'Test',
+      email: 'test@test.com',
+      password: 'hash123',
+      driver_license: 'AB',
+    });
+
+    expect(user).toHaveProperty('id');
+  })
+});
