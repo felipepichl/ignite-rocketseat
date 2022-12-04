a970260d593ad906d5214fb3800bbaff7246a7e3
Felipe Pichl <felipe.pichl@hotmail.com>
2022-12-04 10:51:47 -0300

    Node-JS: Update all dependency

--- node-js/project/src/shared/infra/prisma/prisma-test-environment.ts
+++ node-js/project/src/shared/infra/prisma/prisma-test-environment.ts
@@ -7,7 +7,7 @@ import crypto from 'node:crypto';
 import fs from 'fs';
 import path from 'path';
 
-import type { Config } from '@jest/types';
+import type { EnvironmentContext, JestEnvironmentConfig } from '@jest/environment';
 
 dotenv.config({ path: '.env.testing' });
 
@@ -21,8 +21,8 @@ export default class PrismaTestEnvironment extends NodeEnvironment {
   private prismaLocation: string;
 
 
-  constructor(config: Config.ProjectConfig) {
-    super(config);
+  constructor(config: JestEnvironmentConfig, _context?: EnvironmentContext) {
+    super(config, _context);
 
     this.schema = `test_${crypto.randomUUID()}.db`;
     this.connectionString = `file:./${this.schema}`;
