771ede333f3894aec13ba5b3fc205c951491afa9
Felipe Pichl <felipe.pichl@hotmail.com>
2022-12-04 11:23:20 -0300

    Node-JS: Update ESLint Prettier

--- node-js/project/src/modules/accounts/repositories/in-memory/UserTokensRepositoryInMemory.ts
+++ node-js/project/src/modules/accounts/repositories/in-memory/UserTokensRepositoryInMemory.ts
@@ -1,8 +1,9 @@
 import { ICreateUserTokensDTO } from "@modules/accounts/dtos/ICreateUserTokensDTO";
 import { UserTokens } from "@modules/accounts/infra/prisma/models/UserTokens";
+
 import { IUsersTokensRepository } from "../IUsersTokensRepository";
 
-class UserTokensRepositoryInMemory implements IUsersTokensRepository{
+class UserTokensRepositoryInMemory implements IUsersTokensRepository {
   private usersTokens: UserTokens[] = [];
 
   async create(data: ICreateUserTokensDTO): Promise<UserTokens> {
@@ -15,18 +16,24 @@ class UserTokensRepositoryInMemory implements IUsersTokensRepository{
     return userTokens;
   }
 
-  async findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens> {
-    return this.usersTokens.find(userTokens =>
-      userTokens.fk_user_id === user_id &&
-      userTokens.refresh_token === refresh_token
+  async findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string
+  ): Promise<UserTokens> {
+    return this.usersTokens.find(
+      (userTokens) =>
+        userTokens.fk_user_id === user_id &&
+        userTokens.refresh_token === refresh_token
     );
   }
 
   async deleteById(id: string): Promise<void> {
-    const index = this.usersTokens.findIndex(userTokens => userTokens.id === id);
+    const index = this.usersTokens.findIndex(
+      (userTokens) => userTokens.id === id
+    );
 
     this.usersTokens.splice(index);
   }
 }
 
-export { UserTokensRepositoryInMemory }
+export { UserTokensRepositoryInMemory };
