671b7576db6484e02055b3a30203d436ad07f2a9
Felipe Pichl <felipe.pichl@hotmail.com>
2022-12-20 23:44:05 -0300

    React-Native: P01, M02 - Class 10

--- react-native/project-01/imhere/App.tsx
+++ react-native/project-01/imhere/App.tsx
@@ -1,10 +1,28 @@
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <>
-      <Text>React</Text>
-      <Text>Native</Text>
-    </>
+    <View style={{
+      flex: 1,
+      backgroundColor: "#131016",
+      padding: 24,
+    }}>
+      <Text style={{
+        color: "#fdfcfe",
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+      }}>
+        Event name
+      </Text>
+      <Text style={{
+        color: "#6b6b6b",
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginTop: 48
+      }}>
+        Friday, november 24th 2022
+      </Text>
+    </View>
   )
 }
