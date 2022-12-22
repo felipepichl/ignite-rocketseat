1d6b15c4dfff4dee761931a25821bf81965925c0
Felipe Pichl <felipe.pichl@hotmail.com>
2022-12-21 22:50:58 -0300

    React-Native: P01, M02 - Class 12

--- react-native/project-01/imhere/App.tsx
+++ react-native/project-01/imhere/App.tsx
@@ -1,28 +1,34 @@
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: "#131016",
-      padding: 24,
-    }}>
-      <Text style={{
-        color: "#fdfcfe",
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48
-      }}>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
         Event name
       </Text>
-      <Text style={{
-        color: "#6b6b6b",
-        fontSize: 16,
-        fontWeight: 'bold',
-        marginTop: 48
-      }}>
+      <Text style={styles.eventDate}>
         Friday, november 24th 2022
       </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#fdfcfe",
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: "#6b6b6b",
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 48
+  }
+})
