e6749a90597cf0c6f01ea74cfe156fe93e50a904
Felipe Pichl <felipe.pichl@hotmail.com>
2023-01-16 23:00:05 -0300

    React-Native: P02, M02 - Class 08

--- react-native/project-01/igniteteams/App.tsx
+++ react-native/project-01/igniteteams/App.tsx
@@ -1,14 +1,14 @@
-import { ActivityIndicator } from 'react-native'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components';
 import { 
   useFonts, 
   Roboto_400Regular, 
   Roboto_700Bold 
-} from '@expo-google-fonts/roboto'
+} from '@expo-google-fonts/roboto';
 
-import theme from './src/theme'
+import theme from './src/theme';
 
-import { Groups } from '@screens/Groups'
+import { Loading } from '@components/Loading';
+import { Groups } from '@screens/Groups';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,7 +18,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ?  <Groups /> : <ActivityIndicator /> }
+      { fontsLoaded ?  <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
