7ce7f510d2059e562f85ee5661cfa8b9a84083b9
Felipe Pichl <felipe.pichl@hotmail.com>
2023-01-16 23:22:53 -0300

    React-Native: P02, M02 - Class 10

--- react-native/project-01/igniteteams/src/screens/Groups/index.tsx
+++ react-native/project-01/igniteteams/src/screens/Groups/index.tsx
@@ -1,7 +1,11 @@
 import { Container } from './styles'
 
+import { Header } from '@components/Header';
+
 export function Groups() {
   return (
-    <Container />
+    <Container >
+      <Header />
+    </Container>
   );
 }
