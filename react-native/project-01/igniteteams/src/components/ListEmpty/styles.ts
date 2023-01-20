031c3ed7e067671559bf6476edb38ee114d85309
Felipe Pichl <felipe.pichl@hotmail.com>
2023-01-19 22:52:00 -0300

    React-Native: P02, M02 - Class 16 done

--- react-native/project-01/igniteteams/src/components/ListEmpty/styles.ts
+++ react-native/project-01/igniteteams/src/components/ListEmpty/styles.ts
@@ -10,4 +10,6 @@ export const Message = styled.Text`
   text-align: center;
   
   font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
 `;
--- react-native/project-01/igniteteams/src/screens/Groups/index.tsx
+++ react-native/project-01/igniteteams/src/screens/Groups/index.tsx
@@ -6,6 +6,7 @@ import { Container } from './styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos']);
@@ -25,6 +26,12 @@ export function Groups() {
         renderItem={({ item }) => (
           <GroupCard 
             title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message='Que tal cadastrar uma nova turma'
           />
         )}
       />
