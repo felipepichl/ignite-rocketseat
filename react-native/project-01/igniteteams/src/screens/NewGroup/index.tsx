f7667eef9d14d40fc73aeebe430456dac22f64c5
Felipe Pichl <felipe.pichl@hotmail.com>
2023-01-24 00:03:29 -0300

    React-Native: P02, M02 - Class 19 Part II

--- react-native/project-01/igniteteams/src/screens/NewGroup/index.tsx
+++ react-native/project-01/igniteteams/src/screens/NewGroup/index.tsx
@@ -1,6 +1,8 @@
 import { Container, Content, Icon } from './styles'
 
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { Button } from '@components/Button';
 
 export function NewGroup() {
   return (
@@ -9,6 +11,14 @@ export function NewGroup() {
 
       <Content>
         <Icon />
+        <Highlight 
+          title='Nova Turma'
+          subtitle='crie a turma para adicionar as pessoas'
+        />
+
+        <Button 
+          title='Criar'
+        />
       </Content>
     </Container>
   );
