cca18c954f160a74b8e8adfd69be3de976f60490
Felipe Pichl <felipe.pichl@hotmail.com>
2023-02-02 23:29:50 -0300

    React-Native: P02, M03 - Class 05

--- react-native/project-01/igniteteams/App.tsx
+++ react-native/project-01/igniteteams/App.tsx
@@ -9,7 +9,7 @@ import {
 import theme from './src/theme';
 
 import { Loading } from '@components/Loading';
-import { Groups } from '@screens/Groups';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,7 +24,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      { fontsLoaded ?  <Groups /> : <Loading /> }
+      { fontsLoaded ?  <Routes /> : <Loading /> }
     </ThemeProvider>
   );
 }
