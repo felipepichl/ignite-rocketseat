e0235db239b2a287d12ea6c9e176daf3de159722
Felipe Pichl <felipe.pichl@hotmail.com>
2023-02-07 22:10:12 -0300

    React-Native: P02, M03 - Class 08 done

--- react-native/project-01/igniteteams/src/screens/NewGroup/index.tsx
+++ react-native/project-01/igniteteams/src/screens/NewGroup/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from '@react-navigation/native';
+
 import { Container, Content, Icon } from './styles'
 
 import { Header } from '@components/Header';
@@ -6,9 +8,11 @@ import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 
 export function NewGroup() {
-  
+  const navigation = useNavigation()
+
+
   function handleNew() {
-    
+    navigation.navigate('players', { group: 'Rocket' });    
   }
   
   return (
