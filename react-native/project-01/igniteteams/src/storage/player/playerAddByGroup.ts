8447725a9d20062960648317bde6beb4824c863f
Felipe Pichl <felipe.pichl@hotmail.com>
2023-02-14 23:50:36 -0300

    React-Native: P02, M04 - Class 12 Part I

--- react-native/project-01/igniteteams/src/storage/player/playerAddByGroup.ts
+++ react-native/project-01/igniteteams/src/storage/player/playerAddByGroup.ts
@@ -1,23 +1,26 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AppError } from "@utils/AppError";
 
+import { playerGetByGroup } from './playerGetByGroup';
 import { PLAYER_COLLECTION } from '@storage/storageConfig';
 import { PlayerStorageDTO } from './PlayerStorageDTO';
 
 async function playerAddByGroup(newPlayer: PlayerStorageDTO, group: string) {
   try {
 
-    const storedPlayers = await playersGetAll();
+    const storedPlayers = await playerGetByGroup(group);
 
-    const playerAlreadyExists = storedPlayers.includes(newPlayer);
+    const playerAlreadyExists = storedPlayers.filter(player => 
+      newPlayer.name === player.name
+    );
 
-    if(playerAlreadyExists) {
+    if(playerAlreadyExists.length > 0) {
       throw new AppError('Player already exists')
     }
   
-    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, 
-      [...storedPlayers, newPlayer],
-    );
+    const storage = JSON.stringify([...storedPlayers, newPlayer]);
+
+    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, storage);
   } catch (error) {
     throw (error);
   }
