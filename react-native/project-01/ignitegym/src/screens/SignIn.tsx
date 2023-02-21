8a2a4ff443804a1c3160035cf4a1da4d347f644b
Felipe Pichl <felipe.pichl@hotmail.com>
2023-02-21 07:09:53 -0300

    React-Native: P03, M02 - Class 12

--- react-native/project-01/ignitegym/src/screens/SignIn.tsx
+++ react-native/project-01/ignitegym/src/screens/SignIn.tsx
@@ -4,6 +4,7 @@ import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
 import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 function SignIn() {
   return (
@@ -48,6 +49,10 @@ function SignIn() {
       <Input 
         placeholder='Senha'
         secureTextEntry
+      />
+
+      <Button 
+        title='Acessar'
       />
 
     </VStack>
