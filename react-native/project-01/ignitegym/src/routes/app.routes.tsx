024ce4e4edcb6fc8e168134ca625521447c53024
Felipe Pichl <felipe.pichl@hotmail.com>
2023-02-23 06:28:25 -0300

    React-Native: P03, M04 - Class 07

--- react-native/project-01/ignitegym/src/routes/app.routes.tsx
+++ react-native/project-01/ignitegym/src/routes/app.routes.tsx
@@ -18,7 +18,9 @@ const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 function AppRoutes() {
   return (
-    <Navigator>
+    <Navigator screenOptions={{
+      headerShown: false,
+    }}>
       <Screen name="home" component={Home}/>
       <Screen name='history' component={History}/>
       <Screen name='profile' component={Profile}/>
