0b0b09ae7840c0de772a8b1ce1492d313936deed
Felipe Pichl <felipe.pichl@hotmail.com>
2023-02-23 07:59:16 -0300

    React-Native: P03, M05 - Class 08

--- react-native/project-01/ignitegym/src/components/Group.tsx
+++ react-native/project-01/ignitegym/src/components/Group.tsx
@@ -1,19 +1,31 @@
-import { Text } from 'native-base'
+import { Text, Pressable, IPressableProps } from 'native-base'
 
-type Props = {
+type Props = IPressableProps & {
   name: string;
 }
 
-function Group({ name }: Props) {
+function Group({ name, ...rest }: Props) {
  return (
-  <Text
-    color="gray.200"
-    textTransform='uppercase'
-    fontSize='xs'
-    fontWeight='bold'
+  <Pressable
+    mr={3}
+    w={24}
+    h={10}
+    bg='gray.600'
+    rounded='md'
+    justifyContent='center'
+    alignItems='center'
+    overflow='hidden'
+    {...rest}
   >
-    {name}
-  </Text>
+    <Text
+      color="gray.200"
+      textTransform='uppercase'
+      fontSize='xs'
+      fontWeight='bold'
+    >
+      {name}
+    </Text>
+  </Pressable>
  ); 
 }
 
