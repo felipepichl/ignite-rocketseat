a877cb2365c07727a73b3c2b5f8d8de34b803325
Felipe Pichl <felipe.pichl@hotmail.com>
2023-02-25 07:45:18 -0300

    React-Native: P03, M05 - Class 29

--- react-native/project-01/ignitegym/src/screens/app/Exercise.tsx
+++ react-native/project-01/ignitegym/src/screens/app/Exercise.tsx
@@ -1,12 +1,25 @@
-import { Center, Text } from 'native-base';
+import { TouchableOpacity } from 'react-native'
+import { VStack, Icon } from 'native-base';
+import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
 
 function Exercise() {
+
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
-    <Center
-      flex={1}
-    >
-      <Text>Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px={8} bg='gray.600' pt={12}>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={Feather} name="arrow-left" color="green.500" size={6}/>
+        </TouchableOpacity>
+      </VStack>
+      
+    </VStack >
   );
 }
 
