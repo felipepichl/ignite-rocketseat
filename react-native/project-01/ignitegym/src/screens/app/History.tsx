6d004d496310e54d093161ac57ec8bd6a699c039
Felipe Pichl <felipe.pichl@hotmail.com>
2023-02-25 06:22:37 -0300

    React-Native: P03, M05 - Class 18

--- react-native/project-01/ignitegym/src/screens/app/History.tsx
+++ react-native/project-01/ignitegym/src/screens/app/History.tsx
@@ -1,14 +1,46 @@
-import { VStack } from 'native-base';
+import { useState } from 'react';
+import { Heading, VStack, SectionList } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
 
 function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.22',
+      data: ['Puxada Frontal', 'Remada Unilateral']
+    },
+    {
+      title: '27.08.22',
+      data: ['Puxada Frontal']
+    }
+  ]);
+
   return (
     <VStack
       flex={1}
     >
       <ScreenHeader title="Histórico de Exercícios"/>
+
+      <SectionList 
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mt={10}
+            mb={3}
+          >
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
+
     </VStack>
   );
 }
