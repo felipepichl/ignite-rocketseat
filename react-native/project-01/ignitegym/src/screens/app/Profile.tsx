0e6a6829f01728ba643c4f766801587f24546d92
Felipe Pichl <felipe.pichl@hotmail.com>
2023-02-25 06:59:03 -0300

    React-Native: P03, M05 - Class 23

--- react-native/project-01/ignitegym/src/screens/app/Profile.tsx
+++ react-native/project-01/ignitegym/src/screens/app/Profile.tsx
@@ -1,9 +1,14 @@
-import { Center, ScrollView, VStack } from 'native-base';
+import { useState } from 'react';
+import { Center, ScrollView, VStack, Skeleton } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
 
+const PHOTO_SIZE = 33;
+
 function Profile() {
+  const [photoIsLoading, setPhotoISLoading] = useState(true);
+
   return (
     <VStack
       flex={1}
@@ -12,11 +17,22 @@ function Profile() {
 
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto 
-            source={{ uri: 'https://github.com/felipepichl.png' }}
-            alt="Foto do usuário"
-            size={33}
-          />
+
+          {
+            photoIsLoading ?
+            <Skeleton 
+              w={PHOTO_SIZE} 
+              h={PHOTO_SIZE} 
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          :
+            <UserPhoto 
+              source={{ uri: 'https://github.com/felipepichl.png' }}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />}
         </Center>
 
       </ScrollView>
