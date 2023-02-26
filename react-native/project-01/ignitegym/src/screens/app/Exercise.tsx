a84b9f5e5ee64cb7ebfb317cb7916d9d9dca2556
Felipe Pichl <felipe.pichl@hotmail.com>
2023-02-26 16:47:18 -0300

    React-Native: P03, M05 - Class 32

--- react-native/project-01/ignitegym/src/screens/app/Exercise.tsx
+++ react-native/project-01/ignitegym/src/screens/app/Exercise.tsx
@@ -1,11 +1,15 @@
 import { TouchableOpacity } from 'react-native'
-import { VStack, Icon, HStack, Heading, Text, Image } from 'native-base';
+import { VStack, Icon, HStack, Heading, Text, Image, Box } from 'native-base';
 import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 
 import { AppNavigationRoutesProps } from '@routes/app.routes';
 
 import BodySvg from '@assets/body.svg';
+import SeriesSvg from '@assets/series.svg';
+import RepetitionsSvg from '@assets/repetitions.svg';
+
+import { Button } from '@components/Button';
 
 function Exercise() {
 
@@ -51,6 +55,31 @@ function Exercise() {
           resizeMode="cover"
           rounded="lg"
         />
+
+        <Box bg="gray.600" rounded="md" pb={4} px={4}>
+          <HStack
+            alignItems="center" 
+            justifyContent="space-around"
+            mb={6}
+            mt={5}
+          >
+            <HStack>
+              <SeriesSvg />
+              <Text color="gray.200" ml="2">
+                3 séries
+              </Text>
+            </HStack>
+
+            <HStack>
+              <RepetitionsSvg />
+              <Text color="gray.200" ml="2">
+                12 repetições
+              </Text>
+            </HStack>
+          </HStack>
+
+          <Button title='Marcar como realizado'/>
+        </Box>
       </VStack>
       
     </VStack >
