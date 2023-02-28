2cabfaa5d94bb30ec46f63504796f2d6c935b293
Felipe Pichl <felipe.pichl@hotmail.com>
2023-02-28 06:49:47 -0300

    React-Native: P03, M07 - Class 12

--- react-native/project-01/ignitegym/src/components/Input.tsx
+++ react-native/project-01/ignitegym/src/components/Input.tsx
@@ -1,25 +1,39 @@
-import { Input as NativeBaseInput, IInputProps } from 'native-base';
+import { 
+  Input as NativeBaseInput, 
+  IInputProps,
+  FormControl
+} from 'native-base';
 
+type Props = IInputProps & {
+  errorMessage?: string | null; 
+}
 
-function Input({...rest}: IInputProps) {
+function Input({ errorMessage = null, isInvalid, ...rest}: Props) {
+  const invalid = !!errorMessage || isInvalid;
+  
   return (
-    <NativeBaseInput 
-      bg='gray.700'
-      h={14}
-      px={4}
-      borderWidth={8}
-      fontSize='md'
-      color="white"
-      fontFamily='body'
-      mb={4}
-      placeholderTextColor='gray.300'
-      _focus={{
-        bg: 'gray.700',
-        borderWidth: 1,
-        borderColor: 'green.500'
-      }}
-      {...rest}
-    />
+    <FormControl isInvalid={invalid} mb={4}>
+      <NativeBaseInput 
+        bg='gray.700'
+        h={14}
+        px={4}
+        borderWidth={8}
+        fontSize='md'
+        color="white"
+        fontFamily='body'
+        placeholderTextColor='gray.300'
+        _focus={{
+          bg: 'gray.700',
+          borderWidth: 1,
+          borderColor: 'green.500'
+        }}
+        {...rest}
+      />
+
+      <FormControl.ErrorMessage>
+        {errorMessage}
+      </FormControl.ErrorMessage>
+    </FormControl>
   );
 }
 
