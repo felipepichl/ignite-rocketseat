ccdf3c2fec257988c17e6cae48fe4ac9c409a5b6
Felipe Pichl <felipe.pichl@hotmail.com>
2023-03-02 06:28:53 -0300

    React-Native: RN, M03 - Class 06

--- react-native/project-01/ignitegym/src/services/api.ts
+++ react-native/project-01/ignitegym/src/services/api.ts
@@ -4,4 +4,18 @@ const api = axios.create({
   baseURL: 'http://10.0.0.149:3333'
 });
 
+api.interceptors.request.use((config) => {
+  console.log(config.data);
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+api.interceptors.response.use((response) => {
+  return response;
+}, (error) => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
 export { api };
