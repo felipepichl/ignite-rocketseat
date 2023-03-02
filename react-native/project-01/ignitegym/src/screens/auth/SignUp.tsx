ba4d2d14744af6048e90c81e61f89d68414a58f3
Felipe Pichl <felipe.pichl@hotmail.com>
2023-03-02 06:17:24 -0300

    React-Native: RN, M03 - Class 05

--- react-native/project-01/ignitegym/src/screens/auth/SignUp.tsx
+++ react-native/project-01/ignitegym/src/screens/auth/SignUp.tsx
@@ -1,9 +1,11 @@
+import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver} from '@hookform/resolvers/yup'
 import * as yup from 'yup';
 
+import axios from 'axios';
 import { api } from '@services/api';
 
 import LogoSvg from '@assets/logo.svg'; 
@@ -72,9 +74,14 @@ function SignUp() {
     const data = await response.json();
     console.log(data);
     */
-    
-    const response = await api.post('/users', { name, email, password });
-    console.log(response.data);
+    try {
+      const response = await api.post('/users', { name, email, password });
+      console.log(response.data);
+    } catch (error) {
+      if(axios.isAxiosError(error)){
+        Alert.alert(error.response?.data.message);
+      }
+    }
     
   }
 
