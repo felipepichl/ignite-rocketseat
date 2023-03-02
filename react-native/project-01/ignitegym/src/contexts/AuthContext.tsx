adaa54fc34984970708344ded31742178f9e1bc5
Felipe Pichl <felipe.pichl@hotmail.com>
2023-03-02 07:40:48 -0300

    React-Native: RN, M04 - Class 05

--- react-native/project-01/ignitegym/src/contexts/AuthContext.tsx
+++ react-native/project-01/ignitegym/src/contexts/AuthContext.tsx
@@ -1,10 +1,29 @@
 import { UserDTO } from '@dtos/UserDTO';
-import { createContext } from 'react';
+import { createContext, ReactNode } from 'react';
 
 type AuthContextDataProps = {
   user: UserDTO;
 }
 
+type AuthContextProviderProps = {
+  children: ReactNode;
+}
+
 const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
-export { AuthContext };
+function AuthContextProvider({ children }: AuthContextProviderProps) {
+  return (
+    <AuthContext.Provider value={{
+      user: {
+        id: '1',
+        name: 'Felipe',
+        email: 'felipe@email.com',
+        avatar: 'felipe.png'
+      }
+    }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export { AuthContext, AuthContextDataProps, AuthContextProvider };
