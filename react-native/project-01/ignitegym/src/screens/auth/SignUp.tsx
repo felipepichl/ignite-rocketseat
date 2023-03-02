615bab8f88ad06e789a9050d26ad923db2714bce
Felipe Pichl <felipe.pichl@hotmail.com>
2023-03-02 06:53:26 -0300

    React-Native: RN, M03 - Class 08

--- react-native/project-01/ignitegym/src/screens/auth/SignUp.tsx
+++ react-native/project-01/ignitegym/src/screens/auth/SignUp.tsx
@@ -1,15 +1,23 @@
-import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { 
+  VStack, 
+  Image, 
+  Text, 
+  Center, 
+  Heading, 
+  ScrollView, 
+  useToast 
+} from 'native-base';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver} from '@hookform/resolvers/yup'
 import * as yup from 'yup';
 
-import axios from 'axios';
 import { api } from '@services/api';
 
 import LogoSvg from '@assets/logo.svg'; 
 import BackgroundImg from '@assets/background.png';
+
+import { AppError } from '@utils/AppError';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
@@ -37,6 +45,8 @@ const signUpSchema = yup.object({
 })
 
 function SignUp() {
+
+  const toast = useToast();
 
   const { 
     control, 
@@ -78,9 +88,17 @@ function SignUp() {
       const response = await api.post('/users', { name, email, password });
       console.log(response.data);
     } catch (error) {
-      if(axios.isAxiosError(error)){
-        Alert.alert(error.response?.data.message);
-      }
+      const isAppError = error instanceof AppError;
+      const title = isAppError 
+        ? error.message: 
+          'Não foi possível criar a conta, tente novamente mais tarde';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+
     }
     
   }
--- react-native/project-01/ignitegym/src/services/api.ts
+++ react-native/project-01/ignitegym/src/services/api.ts
@@ -1,9 +1,11 @@
+import { AppError } from '@utils/AppError';
 import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://10.0.0.149:3333'
 });
 
+/*
 api.interceptors.request.use((config) => {
   console.log(config.data);
   return config;
@@ -16,6 +18,16 @@ api.interceptors.response.use((response) => {
 }, (error) => {
   console.log(error);
   return Promise.reject(error);
+});
+*/
+
+api.interceptors.response.use(response => response, error => {
+  if (error.response && error.response.data) {
+    
+    return Promise.reject(new AppError(error.response.data.message));
+  } else {
+    return Promise.reject(error);
+  }
 });
 
 export { api };
