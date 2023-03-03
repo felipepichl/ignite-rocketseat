450353a1faeb9370dbeb9ede1e1169d9cc037984
Felipe Pichl <felipe.pichl@hotmail.com>
2023-03-03 07:57:32 -0300

    React-Native: RN, M04 - Class 16

--- react-native/project-01/ignitegym/src/storage/storageUser.ts
+++ react-native/project-01/ignitegym/src/storage/storageUser.ts
@@ -7,4 +7,12 @@ async function storageUserSave(user: UserDTO) {
   await AsyncStorage.setItem(USER_STORAGE, JSON.stringify(user));
 };
 
-export { storageUserSave };
+async function storageUserGet() {
+  const storage = await AsyncStorage.getItem(USER_STORAGE);
+
+  const user: UserDTO = storage ? JSON.parse(storage) : {};  
+
+  return user;
+};
+
+export { storageUserSave , storageUserGet};
