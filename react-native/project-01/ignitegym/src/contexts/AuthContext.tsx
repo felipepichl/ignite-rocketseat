b9a0fc8bea09edc99b5e451cf85225d0a4434a34
Felipe Pichl <felipe.pichl@hotmail.com>
2023-03-05 08:17:28 -0300

    React-Native: RN, M05 - Class 06

--- react-native/project-01/ignitegym/src/contexts/AuthContext.tsx
+++ react-native/project-01/ignitegym/src/contexts/AuthContext.tsx
@@ -5,6 +5,7 @@ import {
   storageUserGet,
   storageUserRemove,
 } from '@storage/storageUser';
+import { storageAuthTokenSave } from '@storage/storageAuthToken';
 
 import { api } from '@services/api';
 import { UserDTO } from '@dtos/UserDTO';
@@ -26,13 +27,29 @@ function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
 
+  async function storageUserAndToken(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true);
+  
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+      await storageUserSave(userData);
+      await storageAuthTokenSave(token);
+      
+      setUser(user);
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
   async function signIn(email: string, password: string) {
     try {
       const { data } = await api.post('/sessions', { email, password });
       
       if(data.user && data.token) {
-        setUser(data.user);
-        storageUserSave(data.user);
+        storageUserAndToken(data.user, data.token);
       }
 
     } catch (error) {
