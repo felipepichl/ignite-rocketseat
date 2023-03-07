929598c8f04649faddbfebfb3487c1c4f838c130
Felipe Pichl <felipe.pichl@hotmail.com>
2023-03-07 07:04:51 -0300

    React-Native: RN, M06 - Class 12

--- react-native/project-01/ignitegym/src/screens/app/History.tsx
+++ react-native/project-01/ignitegym/src/screens/app/History.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react';
-import { Heading, VStack, SectionList, Text } from 'native-base';
+import { useCallback, useState } from 'react';
+import { Heading, VStack, SectionList, Text, useToast } from 'native-base';
+
+import { api } from '@services/api';
+import { AppError } from '@utils/AppError';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
+import { useFocusEffect } from '@react-navigation/native';
 
 function History() {
+  const [isLoading, setIsLoading] = useState(true);
   const [exercises, setExercises] = useState([
     {
       title: '26.08.22',
@@ -15,6 +20,34 @@ function History() {
       data: ['Puxada Frontal']
     }
   ]);
+
+  const toast = useToast();
+
+  async function fetchHistory() {
+    try {
+      setIsLoading(true);
+
+      const response = await api.get('/history');
+      
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError 
+      ? error.message 
+      : 'Não foi possível carregar o histórico.';
+      
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchHistory();
+  }, []));
 
   return (
     <VStack
