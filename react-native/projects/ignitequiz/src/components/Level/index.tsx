ba06ced10e7cb6aafccff2baa6a848b7000288a5
Felipe Pichl <felipe.pichl@hotmail.com>
2023-03-20 07:28:29 -0300

    React-Native: P05, M02 - Class 06

--- react-native/projects/ignitequiz/src/components/Level/index.tsx
+++ react-native/projects/ignitequiz/src/components/Level/index.tsx
@@ -1,8 +1,10 @@
-import { Pressable, PressableProps, Text } from 'react-native';
+import { useEffect } from 'react';
+import { Pressable, PressableProps } from 'react-native';
 import Animated, { 
   useSharedValue,
   useAnimatedStyle,
-  withTiming
+  withTiming,
+  interpolateColor
 } from 'react-native-reanimated';
 
 import { THEME } from '../../styles/theme';
@@ -22,12 +24,28 @@ type Props = PressableProps & {
 
 export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Props) {
   const scale = useSharedValue(1);
+  const checked = useSharedValue(1);
 
   const COLOR = TYPE_COLORS[type];
 
   const animatedContainerStyle = useAnimatedStyle(() => {
     return {
-      transform: [{ scale: scale.value }]
+      transform: [{ scale: scale.value }],
+      backgroundColor: interpolateColor(
+        checked.value,
+        [0,1],
+        ['transparent', COLOR]
+      )
+    }
+  });
+  
+  const animatedTextStyle = useAnimatedStyle(() => {
+    return {
+      color: interpolateColor(
+        checked.value,
+        [0,1],
+        ['transparent', THEME.COLORS.GREY_100]
+      )
     }
   });
 
@@ -39,6 +57,10 @@ export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Prop
     scale.value = withTiming(1);
   }
 
+  useEffect(() => {
+    checked.value = withTiming(isChecked ? 1 : 0);
+  }, [isChecked])
+
   return (
     <Pressable 
       onPressIn={onPressIn}
@@ -48,17 +70,17 @@ export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Prop
       <Animated.View style={
         [
           styles.container,
+          { borderColor: COLOR },
           animatedContainerStyle,
-          { borderColor: COLOR, backgroundColor: isChecked ? COLOR : 'transparent' }
         ]
       }>
-        <Text style={
+        <Animated.Text style={
           [
             styles.title,
-            { color: isChecked ? THEME.COLORS.GREY_100 : COLOR }
+            animatedTextStyle
           ]}>
           {title}
-        </Text>
+        </Animated.Text>
       </Animated.View>
     </Pressable>
   );
