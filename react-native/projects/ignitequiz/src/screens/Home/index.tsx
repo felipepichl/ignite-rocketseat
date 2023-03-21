7b6e144ac7eccd542b27053432457fc8fb842f4e
Felipe Pichl <felipe.pichl@hotmail.com>
2023-03-21 07:41:22 -0300

    React-Native: P05, M02 - Class 10

--- react-native/projects/ignitequiz/src/screens/Home/index.tsx
+++ react-native/projects/ignitequiz/src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { FlatList, View } from 'react-native';
 import { Trophy } from 'phosphor-react-native';
 import { useNavigation } from '@react-navigation/native';
+import Animated from 'react-native-reanimated';
 
 import { Level } from '../../components/Level';
 import { Header } from '../../components/Header';
@@ -41,25 +42,28 @@ export function Home() {
         onPress={() => navigate('history')}
       />
 
-      <View style={styles.levels}>
-        <Level title="Fácil" type="EASY" onPress={() => handleLevelFilter(1)} isChecked={levels.includes(1)} />
-        <Level title="Médio" type="MEDIUM" onPress={() => handleLevelFilter(2)} isChecked={levels.includes(2)} />
-        <Level title="Difícil" type="HARD" onPress={() => handleLevelFilter(3)} isChecked={levels.includes(3)} />
-      </View>
+      <Animated.View>
+        <View style={styles.levels}>
+          <Level title="Fácil" type="EASY" onPress={() => handleLevelFilter(1)} isChecked={levels.includes(1)} />
+          <Level title="Médio" type="MEDIUM" onPress={() => handleLevelFilter(2)} isChecked={levels.includes(2)} />
+          <Level title="Difícil" type="HARD" onPress={() => handleLevelFilter(3)} isChecked={levels.includes(3)} />
+        </View>
 
-      <FlatList
-        data={quizzes}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <QuizCard
-            data={item}
-            onPress={() => navigate('quiz', { id: item.id })}
-          />
-        )}
-        numColumns={2}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.cards}
-      />
+        <FlatList
+          data={quizzes}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <QuizCard
+              data={item}
+              onPress={() => navigate('quiz', { id: item.id })}
+            />
+          )}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.cards}
+        />
+      </Animated.View>
+
     </View>
   );
 }
