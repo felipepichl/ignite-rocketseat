46895b9819a635bacddff2fe56739e13a2772bfb
Felipe Pichl <felipe.pichl@hotmail.com>
2023-03-22 06:41:02 -0300

    React-Native: P05, M02 - Class 13

--- react-native/projects/ignitequiz/src/components/Question/index.tsx
+++ react-native/projects/ignitequiz/src/components/Question/index.tsx
@@ -59,8 +59,8 @@ export function Question({ question, alternativeSelected, setAlternativeSelected
   return (
     <Animated.View 
       style={styles.container}
-      entering={enteringKeyframe} 
-      exiting={exitingKeyframe} 
+      entering={enteringKeyframe.delay(400)} 
+      exiting={exitingKeyframe.delay(400)} 
     >
       <Text style={styles.title}>
         {question.title}
