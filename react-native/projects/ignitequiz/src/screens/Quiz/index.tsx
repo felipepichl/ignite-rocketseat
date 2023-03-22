bab6d67a4fda9591358df4cc1b26d88b5c025412
Felipe Pichl <felipe.pichl@hotmail.com>
2023-03-22 07:58:47 -0300

    React-Native: P05, M03 - Class 08

--- react-native/projects/ignitequiz/src/screens/Quiz/index.tsx
+++ react-native/projects/ignitequiz/src/screens/Quiz/index.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
-import { Alert, ScrollView, View } from 'react-native';
+import { Alert, View } from 'react-native';
 import Animated, { 
   useAnimatedStyle,
   useSharedValue, 
   withSequence, 
   withTiming,
   interpolate, 
-  Easing
+  Easing,
+  useAnimatedScrollHandler
 } from 'react-native-reanimated';
 
 import { useNavigation, useRoute } from '@react-navigation/native';
@@ -36,6 +37,7 @@ export function Quiz() {
   const [alternativeSelected, setAlternativeSelected] = useState<null | number>(null);
 
   const shake = useSharedValue(0);
+  const scrollY = useSharedValue(0);
 
   const { navigate } = useNavigation();
 
@@ -123,6 +125,12 @@ export function Quiz() {
     }
   });
 
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      scrollY.value = event.contentOffset.y;
+    }
+  })
+
   useEffect(() => {
     const quizSelected = QUIZ.filter(item => item.id === id)[0];
     setQuiz(quizSelected);
@@ -135,9 +143,11 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
-      <ScrollView
+      <Animated.ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.question}
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
       >
         <QuizHeader
           title={quiz.title}
@@ -158,7 +168,7 @@ export function Quiz() {
           <OutlineButton title="Parar" onPress={handleStop} />
           <ConfirmButton onPress={handleConfirm} />
         </View>
-      </ScrollView>
+      </Animated.ScrollView>
     </View >
   );
 }
