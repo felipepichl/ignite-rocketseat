9b97b68b5e6685053d6e1602f8b3be11ea33f530
Felipe Pichl <felipe.pichl@hotmail.com>
2023-03-23 07:53:40 -0300

    React-Native: P05, M03 - Class 13

--- react-native/projects/ignitequiz/src/screens/Quiz/index.tsx
+++ react-native/projects/ignitequiz/src/screens/Quiz/index.tsx
@@ -8,7 +8,8 @@ import Animated, {
   interpolate, 
   Easing,
   useAnimatedScrollHandler,
-  Extrapolate
+  Extrapolate,
+  runOnJS
 } from 'react-native-reanimated';
 import { GestureDetector, Gesture } from 'react-native-gesture-handler';
 
@@ -34,6 +35,7 @@ interface Params {
 type QuizProps = typeof QUIZ[0];
 
 const CARD_INCLINATION = 10;
+const CARD_SKIP_AREA = (-200);
 
 export function Quiz() {
   const [points, setPoints] = useState(0);
@@ -170,7 +172,10 @@ export function Quiz() {
         cardPosition.value = event.translationX;
       }
     })
-    .onEnd(() => {
+    .onEnd((event) => {
+      if (event.translationX < CARD_SKIP_AREA) {
+        runOnJS(handleSkipConfirm)();
+      }
       cardPosition.value = withTiming(0);
     });
   
