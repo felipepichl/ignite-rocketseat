a2fb69eb63a1bed9e9e7c94a7b1e8bed610fc367
Felipe Pichl <felipe.pichl@hotmail.com>
2023-04-18 07:51:54 -0300

    RN-2021: CII-AS, M01 - Class 13 Part I

--- react-native/trail-2021/projects/gofinances/src/screens/Resume/index.tsx
+++ react-native/trail-2021/projects/gofinances/src/screens/Resume/index.tsx
@@ -3,7 +3,7 @@ import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { VictoryPie } from 'victory-native';
 import { RFValue } from 'react-native-responsive-fontsize';
-
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
 import { useTheme } from 'styled-components';
 
 import { 
@@ -99,7 +99,13 @@ export function Resume() {
         <Title>Resumo por categoria</Title>
       </Header>
 
-      <Content>
+      <Content
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 24,
+          paddingBottom: useBottomTabBarHeight(),
+        }}
+      >
         <ChartContainer>
           <VictoryPie 
             data={totalByCategories}
