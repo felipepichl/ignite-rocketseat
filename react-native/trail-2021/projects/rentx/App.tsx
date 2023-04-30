72d16302f0f70a14c5d6ab5005193daa5feb6357
Felipe Pichl <felipe.pichl@hotmail.com>
2023-04-30 12:00:13 -0300

    RN-2021: CII-CA, M01 - Class 04

--- react-native/trail-2021/projects/rentx/App.tsx
+++ react-native/trail-2021/projects/rentx/App.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo'
+
+import theme from './src/styles/theme';
 
 import { Home } from './src/screens/app/Home';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      {
+        fontsLoaded 
+        ? <Home/>
+        : <ActivityIndicator />
+      }
+    </ThemeProvider>
   );
 }
