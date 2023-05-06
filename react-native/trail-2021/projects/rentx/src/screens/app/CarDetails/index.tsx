71eda7ea0722957c5379fc8992c6ab439d9187f2
Felipe Pichl <felipe.pichl@hotmail.com>
2023-05-06 06:51:52 -0300

    RN-2021: CII-CA, M01 - Class 10 done

--- react-native/trail-2021/projects/rentx/src/screens/app/CarDetails/index.tsx
+++ react-native/trail-2021/projects/rentx/src/screens/app/CarDetails/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
+import { BackButton } from '../../../components/BackButton';
+import { ImageSlider } from '../../../components/ImageSlider';
+import { Accessory } from '../../../components/Accessory';
+
+import speedSvg from '../../../assets/speed.svg';
+import accelerationSvg from '../../../assets/acceleration.svg';
+import forceSvg from '../../../assets/force.svg';
+import gasolineSvg from '../../../assets/gasoline.svg';
+import exchangeSvg from '../../../assets/exchange.svg';
+import peopleSvg from '../../../assets/people.svg';
+
+
 import { 
   Container, 
   Header,
@@ -12,12 +24,9 @@ import {
   Rent,
   Period,
   Price, 
-  About
+  About,
+  Accessories
 } from './styles';
-
-import { BackButton } from '../../../components/BackButton';
-import { ImageSlider } from '../../../components/ImageSlider';
-import { Accessory } from '../../../components/Accessory';
 
 export function CarDetails() {
   return (
@@ -45,7 +54,14 @@ export function CarDetails() {
           </Rent>
         </Details>
 
-        {/* <Accessory /> */}
+        <Accessories>
+          <Accessory name='380Km/h' icon={speedSvg}/>
+          <Accessory name='3.2s' icon={accelerationSvg}/>
+          <Accessory name='800 HP' icon={forceSvg}/>
+          <Accessory name='Gasolina' icon={gasolineSvg}/>
+          <Accessory name='Auto' icon={exchangeSvg}/>
+          <Accessory name='2 Pessoas' icon={peopleSvg}/>
+        </Accessories>
 
         <About>
           A nice car, everything you need to get more girls
