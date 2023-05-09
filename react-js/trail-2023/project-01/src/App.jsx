e8396be109efa08c26ed1754bf4d090f7ae5d16e
Felipe Pichl <felipe.pichl@hotmail.com>
2023-05-09 07:42:16 -0300

    RJ-2023: P01, M02 - Class 01

--- react-js/trail-2023/project-01/src/App.jsx
+++ react-js/trail-2023/project-01/src/App.jsx
@@ -1,8 +1,16 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header"
 
 export function App() {
   return (
-    <Post/>
+    <>
+      <Header />
+      <Post
+        author="Felipe Pichl"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, officiis quo maiores eaque inventore vero laborum! Repellendus dolor provident porro eum omnis, fuga fugit autem veritatis cum modi molestiae fugiat!"
+      />
+    </>
   )
 }
+
 
