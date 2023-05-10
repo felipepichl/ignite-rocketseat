82c28d39ed39b6bc0b1224e99234d2a289f059ae
Felipe Pichl <felipe.pichl@hotmail.com>
2023-05-10 07:35:34 -0300

    RJ-2023: P01, M02 - Class 4 Part I

--- react-js/trail-2023/project-01/src/App.jsx
+++ react-js/trail-2023/project-01/src/App.jsx
@@ -1,17 +1,27 @@
-import { Post } from "./Post"
-import { Header } from "./components/Header"
+import { Post } from "./Post";
+import { Header } from "./components/Header";
 
-import './styles/global.css'
+import styles from './App.module.css';
+
+import './styles/global.css';
 
 export function App() {
   return (
-    <>
+    <div>
       <Header />
-      <Post
-        author="Felipe Pichl"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, officiis quo maiores eaque inventore vero laborum! Repellendus dolor provident porro eum omnis, fuga fugit autem veritatis cum modi molestiae fugiat!"
-      />
-    </>
+      
+      <div className={styles.wrapper}>
+        <aside>
+          Sidebar
+        </aside>
+        <main>
+        <Post
+          author="Felipe Pichl"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, officiis quo maiores eaque inventore vero laborum! Repellendus dolor provident porro eum omnis, fuga fugit autem veritatis cum modi molestiae fugiat!"
+        />
+        </main>
+      </div>
+    </div>
   )
 }
 
---
+++ react-js/trail-2023/project-01/src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <h1>sidebar</h1>
+  )
+}
