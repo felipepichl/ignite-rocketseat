42a2a4af35bebfb0c1a6512165ce840ac243ad74
Felipe Pichl <felipe.pichl@hotmail.com>
2023-05-13 07:36:45 -0300

    RJ-2023: P01, M02 - Class 06 done

--- react-js/trail-2023/project-01/src/components/Post.jsx
+++ react-js/trail-2023/project-01/src/components/Post.jsx
@@ -2,6 +2,29 @@ import styles from './Post.module.css';
 
 export function Post() {
   return (
-    <h1>Post</h1>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img 
+            className={styles.avatar}
+            src="https://github.com/felipepichl.png"   
+          />
+          <div className={styles.authorInfo}>
+            <strong>Felipe Pichl</strong>
+            <span>Manager</span>
+          </div>
+        </div>
+
+        <time 
+         title="11 de Maio de às 11h"
+         dateTime=''>Publicado há 1h
+        </time>
+
+        <div className={styles.content}>
+          Post content
+
+        </div>
+      </header>
+    </article>
   )
 }
