ca2905c1cce67867caf3ca9eef914a9084594d8d
Felipe Pichl <felipe.pichl@hotmail.com>
2023-05-16 07:29:38 -0300

    RJ-2023: P01, M02 - Class 09 done

--- react-js/trail-2023/project-01/src/components/Comment.jsx
+++ react-js/trail-2023/project-01/src/components/Comment.jsx
@@ -1,7 +1,40 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 
 export function Comment() {
   return (
-    <h1>comment</h1>
+    <div className={styles.comment}>
+      <img 
+      
+        src="https://github.com/felipepichl.png"   
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Felipe Pichl</strong>
+              <time 
+                title="11 de Maio de às 11h"
+                dateTime=''>Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20}/>
+            </button>
+          </header>
+
+          <p>Very good, congratulations</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
   )
 }
