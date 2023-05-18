d1eb98e37f2c7f3d21cebfcdbd844412b7fcf4dd
Felipe Pichl <felipe.pichl@hotmail.com>
2023-05-18 08:01:17 -0300

    RJ-2023: P01, M03 - Class 01 Part I

--- react-js/trail-2023/project-01/src/App.jsx
+++ react-js/trail-2023/project-01/src/App.jsx
@@ -6,6 +6,35 @@ import styles from './App.module.css';
 
 import './styles/global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      authorUrl: 'https://github.com/felipepichl.png',
+      role: 'Manager'
+    },
+    content: [
+      { type: 'paragraph', content: 'Hello guys' },
+      { type: 'paragraph', content: 'This is my new post about cool things' },
+      { type: 'link', content: 'github.com/felipepichl' },
+    ],
+    publishedAt: new Date('2023-05-03 07:50:00')
+  },
+  {
+    id: 2,
+    author: {
+      authorUrl: 'https://github.com/diego3g.png',
+      role: 'CTO @ Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Say my brother Dev' },
+      { type: 'paragraph', content: 'Now you learn about ReactJS' },
+      { type: 'link', content: 'github.com/diego3g' },
+    ],
+    publishedAt: new Date('2023-05-03 15:00:00')
+  }
+]
+
 export function App() {
   return (
     <div>
