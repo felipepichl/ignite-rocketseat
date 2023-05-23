03adaf7dc1917e6a9c715a52317fa123227941d0
Felipe Pichl <felipe.pichl@hotmail.com>
2023-05-22 21:10:51 -0300

    RJ-2023: P01, M03 - Class 04 done

--- react-js/trail-2023/project-01/src/components/Post.jsx
+++ react-js/trail-2023/project-01/src/components/Post.jsx
@@ -9,6 +9,7 @@ import { useState } from 'react';
 
 export function Post({ author, publishedAt, content }) {
   const [comments, setComments] = useState(['Nice post!!']);
+  const [newCommentText, setNewCommentText] = useState();
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR
@@ -22,11 +23,12 @@ export function Post({ author, publishedAt, content }) {
   function handleCreateNewComment() {
     event.preventDefault();
 
-    const newCommentText = event.target.comment.value;
-
     setComments([...comments, newCommentText]);
+    setNewCommentText('')
+  }
 
-    event.target.comment.value = '';
+  function handleCreateNewCommentChange() {
+    setNewCommentText(event.target.value);
   }
 
   return (
@@ -68,6 +70,8 @@ export function Post({ author, publishedAt, content }) {
         <textarea 
           name='comment'
           placeholder='Deixe um comentário'
+          value={newCommentText}
+          onChange={handleCreateNewCommentChange}
         />
 
         <footer>
