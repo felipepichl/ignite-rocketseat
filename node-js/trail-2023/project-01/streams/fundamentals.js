558dca4d0674e4f43bcabfcc2bcdf44e7b354fdd
Felipe Pichl <felipe.pichl@hotmail.com>
2023-05-31 07:35:18 -0300

    NJS-2023: P01, M03 - Class 03

--- node-js/trail-2023/project-01/streams/fundamentals.js
+++ node-js/trail-2023/project-01/streams/fundamentals.js
@@ -2,7 +2,7 @@
 // process.stdin
 //   .pipe(process.stdout)
 
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHoundredStream extends Readable {
   index = 1
@@ -21,4 +21,22 @@ class OneToHoundredStream extends Readable {
   }
 }
 
-new OneToHoundredStream().pipe(process.stdout)
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback()
+  }
+}
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+    
+    callback(null, Buffer.from(transformed))
+  }
+}
+
+// new OneToHoundredStream().pipe(process.stdout)
+new OneToHoundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
