15d680976828f78ceadfea9ae7d8387ccbd856a3
Felipe Pichl <felipe.pichl@hotmail.com>
2023-07-20 00:05:12 -0300

    RJ-2023: P01, M03 - Class 09

--- react-js/trail-2023/project-01/src/components/Comment.jsx
+++ react-js/trail-2023/project-01/src/components/Comment.jsx
@@ -1,12 +1,20 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
 
   function handleDeleteComment() {
     onDeleteComment(content);
   }
+  
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1)    
+  }
+
+
 
   return (
     <div className={styles.comment}>
@@ -32,9 +40,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
