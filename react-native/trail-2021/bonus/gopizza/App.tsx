0e6267a65355d065155f7ad6d952c417ce2c6257
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-09 20:21:55 -0300

    RN-2021: Bonus, M01 - Class 08

--- react-native/trail-2021/bonus/gopizza/App.tsx
+++ react-native/trail-2021/bonus/gopizza/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+import { ThemeProvider } from 'styled-components/native';
+
+import theme from './src/theme'
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -15,8 +17,8 @@ export default function App() {
   }
 
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      
+    </ThemeProvider >
   );
 }
