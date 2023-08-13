c0b9fda1d775732bf3265fd1b15e29b2d3921df7
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-12 23:21:23 -0300

    RN-2021: Bonus, M02 - Class 06

--- react-native/trail-2021/bonus/gopizza/src/screens/SignIn/index.tsx
+++ react-native/trail-2021/bonus/gopizza/src/screens/SignIn/index.tsx
@@ -1,29 +1,60 @@
 import React from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+
+import brandImg from '@assets/brand.png';
 
 import { Input } from '@components/input';
 import { Button } from '@components/Button';
 
-import { Container } from './styles'
+import { 
+  Container,
+  Content,
+  Title,
+  Brand,
+  ForgotPasswordButton,
+  ForgotPasswordLabel
+} from './styles';
 
 export function SignIn(){
   return (
     <Container>
-      <Input 
-        placeholder='E-mail'
-        type='secondary'
-        autoCorrect={false}
-        autoCapitalize='none'
-      />
-      <Input 
-        placeholder='Senha'
-        type='secondary'
-        secureTextEntry
-      />
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      
+      >
+        <Content>
 
-      <Button 
-        title='Entrar'
-        type='secondary'
-      />
+          <Brand 
+            source={brandImg}
+          />
+
+          <Title>Login</Title>
+          
+          <Input 
+            placeholder='E-mail'
+            type='secondary'
+            autoCorrect={false}
+            autoCapitalize='none'
+          />
+          
+          <Input 
+            placeholder='Senha'
+            type='secondary'
+            secureTextEntry
+          />
+
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel>
+              Esqueci minha senha
+            </ForgotPasswordLabel>
+          </ForgotPasswordButton>
+
+          <Button 
+            title='Entrar'
+            type='secondary'
+          />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   )
 }
