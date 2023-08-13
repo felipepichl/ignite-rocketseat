5323806c84a8c2a53b0d7941c4dcd1153ee5f50d
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-13 00:51:52 -0300

    RN-2021: Bonus, M02 - Class 10

--- react-native/trail-2021/bonus/gopizza/src/hooks/auth.tsx
+++ react-native/trail-2021/bonus/gopizza/src/hooks/auth.tsx
@@ -4,9 +4,12 @@ import React, {
   useState,
   ReactNode
 } from 'react';
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 type AuthContextData = {
-
+  signIn: (email: string, password: string) => Promise<void>;
+  isLogging: boolean;
 }
 
 type AuthProviderProps = {
@@ -16,8 +19,37 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData); 
 
 function AuthProvider({ children }: AuthProviderProps) {
+  const [isLogging, setIsLogging] = useState(false);
+
+  async function signIn(email:string, password: string) {
+    if (!email || !password) {
+      Alert.alert('Login', 'Informe o email ou a senha');
+    }
+
+    setIsLogging(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(account => {
+        console.log(account)
+      })
+      .catch(err => {
+        const { code } = err;
+
+        if (code === 'auth/user-not-found' || code === 'auth/wrong-password') {
+          Alert.alert('Login', 'Email e/ou senha inválido');
+        } else {
+          Alert.alert('Login', 'Não foi possível realizar o login');
+        }
+      }).finally(() => setIsLogging(false));
+
+  }
+
   return (
-    <AuthContext.Provider value={{}}>
+    <AuthContext.Provider value={{
+      signIn,
+      isLogging
+    }}>
       {children}
     </AuthContext.Provider>
   )
