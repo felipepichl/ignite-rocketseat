70ed66277c6fa0884ebb7269e27746d459920530
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-20 02:38:24 -0300

    RN-2021: Bonus, M03 - Class 07

--- react-native/trail-2021/bonus/gopizza/src/screens/Product/index.tsx
+++ react-native/trail-2021/bonus/gopizza/src/screens/Product/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Platform, TouchableOpacity } from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
 
 import { ButtonBack } from '@src/components/ButtonBack'
 import { Photo } from '@components/Photo'
@@ -14,6 +15,24 @@ import {
 } from './styles'
 
 export function Product() {
+  const [image, setImage] = useState('')
+  
+  async function handlePickerImage() {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if(status === 'granted') {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 4]
+      })
+
+      if (!result.canceled) {
+        setImage(result.assets.toString())
+      }
+    }
+    
+  }
+
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Header>
@@ -27,8 +46,13 @@ export function Product() {
       </Header>
 
       <Upload>
-        <Photo uri=""/>
-        <PickImageButton title='Carregar' type='secondary'/>
+        <Photo uri={image}/>
+        
+        <PickImageButton 
+          title='Carregar'
+          type='secondary'
+          onPress={handlePickerImage}
+        />
       </Upload>
     </Container>
   )
