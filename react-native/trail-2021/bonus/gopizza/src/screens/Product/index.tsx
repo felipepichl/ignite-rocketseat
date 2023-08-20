45db952795d3c24da268bff6d9d186405e501ae7
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-20 00:34:04 -0300

    RN-2021: Bonus, M03 - Class 03

--- react-native/trail-2021/bonus/gopizza/src/screens/Product/index.tsx
+++ react-native/trail-2021/bonus/gopizza/src/screens/Product/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Platform, TouchableOpacity } from 'react-native'
 
+import { ButtonBack } from '@src/components/ButtonBack';
+
 import { Container, Header, Title, DeleteLable } from './styles';
 
 export function Product() {
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Header>
+        <ButtonBack />
+
         <Title>Cadastrar</Title>
+        
         <TouchableOpacity>
           <DeleteLable>Deletar</DeleteLable>
         </TouchableOpacity>
