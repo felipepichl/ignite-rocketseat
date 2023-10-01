e62086131054daa96160652544c005cfe877e289
Felipe Pichl <felipe.pichl@hotmail.com>
2023-10-01 11:16:42 -0300

    RN-TA: P06, M02 - Class 04 Part I

--- react-native/projects/ignitefleet/App.tsx
+++ react-native/projects/ignitefleet/App.tsx
@@ -1,9 +1,27 @@
 import { ThemeProvider } from 'styled-components/native'
-import { SignIn } from './src/screens/SignIn'
+import { 
+  useFonts, 
+  Roboto_400Regular, 
+  Roboto_700Bold 
+} from '@expo-google-fonts/roboto'
 
 import theme from './src/theme'
 
+import { SignIn } from './src/screens/SignIn'
+import { Loading } from './src/components/Loading'
+
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return (
+      <Loading/>
+    )
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <SignIn />
