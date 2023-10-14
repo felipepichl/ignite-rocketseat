90c7b2571fe940d71e1d4ea3f454bb98afe59e0c
Felipe Pichl <felipe.pichl@hotmail.com>
2023-10-14 14:53:25 -0300

    RN-TA: P06, M03 - Class 04 Done

--- react-native/projects/ignitefleet/src/screens/SignIn/index.tsx
+++ react-native/projects/ignitefleet/src/screens/SignIn/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 
 import * as WebBrowser from 'expo-web-browser'
 import * as Google from 'expo-auth-session/providers/google'
+import * as AuthSession from 'expo-auth-session';
 
 import { Container, Title, Slogan } from './styles'
 
@@ -12,14 +13,13 @@ import { Button } from '../../components/Button'
 import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env'
 
 WebBrowser.maybeCompleteAuthSession()
+const redirectUri = AuthSession.makeRedirectUri();
 
 export function SignIn() {
   const [isAuthenticating, setIsAuthenticating] = useState(false)
 
-  console.log(ANDROID_CLIENT_ID)
-
   const [_, response, googleSignIn] = Google.useAuthRequest({
-    androidClientId: ANDROID_CLIENT_ID,
+    androidClientId: IOS_CLIENT_ID,
     iosClientId: IOS_CLIENT_ID,
     scopes: ['profile', 'email']
   })
