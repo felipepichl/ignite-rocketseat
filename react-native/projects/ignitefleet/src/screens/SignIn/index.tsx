b34cae7b05cbec7a094cb48bd1368abf69ba7053
Felipe Pichl <felipe.pichl@hotmail.com>
2023-10-14 15:09:03 -0300

    RN-TA: P06, M03 - Class 05

--- react-native/projects/ignitefleet/src/screens/SignIn/index.tsx
+++ react-native/projects/ignitefleet/src/screens/SignIn/index.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
+import { Alert } from 'react-native';
 
 import * as WebBrowser from 'expo-web-browser'
 import * as Google from 'expo-auth-session/providers/google'
-import * as AuthSession from 'expo-auth-session';
 
 import { Container, Title, Slogan } from './styles'
 
@@ -13,7 +13,6 @@ import { Button } from '../../components/Button'
 import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env'
 
 WebBrowser.maybeCompleteAuthSession()
-const redirectUri = AuthSession.makeRedirectUri();
 
 export function SignIn() {
   const [isAuthenticating, setIsAuthenticating] = useState(false)
@@ -37,7 +36,17 @@ export function SignIn() {
   useEffect(() => {
     if (response?.type === 'success') {
       if (response.authentication?.idToken) {
-        console.log('Token => ', response.authentication.idToken)
+
+        const { idToken } = response.authentication
+        
+        fetch(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${idToken}`)
+          .then(response => response.json())
+          .then(console.log)
+
+          
+      } else {
+        Alert.alert('Entar', 'Não foi possível conectar-se a sua conta Google.')
+        setIsAuthenticating(false)
       }
     }
   }, [response])
