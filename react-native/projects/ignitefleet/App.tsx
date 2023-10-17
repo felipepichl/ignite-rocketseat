af631678dd560ec35571210ef58b1bec0498ec0d
Felipe Pichl <felipe.pichl@hotmail.com>
2023-10-16 21:32:30 -0300

    RN-TA: P06, M04 - Class 03

--- react-native/projects/ignitefleet/App.tsx
+++ react-native/projects/ignitefleet/App.tsx
@@ -4,12 +4,16 @@ import {
   Roboto_400Regular, 
   Roboto_700Bold 
 } from '@expo-google-fonts/roboto'
+import { StatusBar } from 'react-native'
+
+import { AppProvider, UserProvider } from '@realm/react'
+import { REALM_APP_ID } from '@env'
 
 import theme from './src/theme'
 
-import { SignIn } from './src/screens/SignIn'
 import { Loading } from './src/components/Loading'
-import { StatusBar } from 'react-native'
+import { SignIn } from './src/screens/SignIn'
+import { Home } from './src/screens/Home'
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -24,13 +28,17 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar 
-        barStyle='light-content'
-        backgroundColor='transparent'
-        translucent
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider  id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar 
+          barStyle='light-content'
+          backgroundColor='transparent'
+          translucent
+        />
+        <UserProvider fallback={SignIn}>
+          <Home/>
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
