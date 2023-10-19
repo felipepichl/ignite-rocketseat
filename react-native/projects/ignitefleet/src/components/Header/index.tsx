2343c5f95498fc3ad88307cd9b6aff65943ddeb7
Felipe Pichl <felipe.pichl@hotmail.com>
2023-10-18 21:52:46 -0300

    RN-TA: P06, M04 - Class 06

--- react-native/projects/ignitefleet/src/components/Header/index.tsx
+++ react-native/projects/ignitefleet/src/components/Header/index.tsx
@@ -1,3 +1,8 @@
+import { TouchableOpacity } from 'react-native'
+import { Power } from 'phosphor-react-native'
+
+import theme from '../../theme'
+
 import { Container, Greeting, Message, Name } from './styles'
 
 export function Header() {
@@ -8,6 +13,13 @@ export function Header() {
 
         <Name>Felipe</Name>
       </Greeting>
+
+      <TouchableOpacity>
+        <Power 
+          size={32}
+          color={theme.COLORS.GRAY_700}
+        />
+      </TouchableOpacity>
     </Container>
   )
 }
