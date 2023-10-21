ecac83d47ec65ec6314590fd5bca8061f8494fbc
Felipe Pichl <felipe.pichl@hotmail.com>
2023-10-21 09:55:48 -0300

    RJ-2023: P02, M01 - Class 04

--- react-js/trail-2023/project-02/src/App.tsx
+++ react-js/trail-2023/project-02/src/App.tsx
@@ -1,7 +1,10 @@
+import {ThemeProvider} from 'styled-components'
+import { defaultTheme } from '../src/styles/themes/detault'
+
 export function App() {  
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello World</h1>
-    </>
+    </ThemeProvider>
   )
 }
