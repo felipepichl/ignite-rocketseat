717f6bcafb7bbfc590148574521be9169e415250
Felipe Pichl <felipe.pichl@hotmail.com>
2023-11-25 22:02:13 -0300

    RN-TA: P06, M01 - Class 06

--- react-native/projects/ignitefleet/App.tsx
+++ react-native/projects/ignitefleet/App.tsx
@@ -1,21 +1,22 @@
 import { ThemeProvider } from 'styled-components/native'
+import { StatusBar } from 'react-native'
+import { AppProvider, UserProvider } from '@realm/react'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { 
   useFonts, 
   Roboto_400Regular, 
   Roboto_700Bold 
 } from '@expo-google-fonts/roboto'
-import { StatusBar } from 'react-native'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
-
-import { AppProvider, UserProvider } from '@realm/react'
-import { REALM_APP_ID } from '@env'
 
 import theme from './src/theme'
 
-import { Loading } from './src/components/Loading'
-import { SignIn } from './src/screens/SignIn'
+import { REALM_APP_ID } from '@env'
 
 import { Routes } from './src/routes'
+import { RealmProvider } from './src/libs/realm'
+
+import { Loading } from './src/components/Loading'
+import { SignIn } from './src/screens/SignIn'
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -39,7 +40,9 @@ export default function App() {
               translucent
             />
             <UserProvider fallback={SignIn}>
-              <Routes/>
+              <RealmProvider>
+                <Routes/>
+              </RealmProvider>
             </UserProvider>
           </SafeAreaProvider>
         </ThemeProvider>
