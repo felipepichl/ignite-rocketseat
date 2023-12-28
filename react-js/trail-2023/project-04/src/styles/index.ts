70bb0395214c29b8b29ee48547b9f72c4d40ae57
Felipe Pichl <felipe.pichl@hotmail.com>
2023-12-27 22:08:27 -0300

    RJ-2023: P04, M02 - Class 02

--- react-js/trail-2023/project-04/src/styles/index.ts
+++ react-js/trail-2023/project-04/src/styles/index.ts
@@ -12,7 +12,15 @@ export const {
 } = createStitches({
   theme: {
     colors: {
-      belle: '#7159c1'
+      white: '#FFF',
+
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e'
     }
   }
 })
