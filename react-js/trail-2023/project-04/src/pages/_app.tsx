6d5da320f5aeda8afeac669752b7e21b549daf55
Felipe Pichl <felipe.pichl@hotmail.com>
2023-12-28 23:16:55 -0300

    RJ-2023: P04, M02 - Class 04

--- react-js/trail-2023/project-04/src/pages/_app.tsx
+++ react-js/trail-2023/project-04/src/pages/_app.tsx
@@ -5,13 +5,15 @@ import { globalStyles } from '@/styles/global'
 import logo from '@/assets/logo.svg'
 import { Container, Header } from '@/styles/pages/app'
 
+import Image from 'next/image'
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logo.src} alt="" />
+        <Image src={logo} alt="" />
       </Header>
 
       <Component {...pageProps} />
