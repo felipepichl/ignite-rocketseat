4aa22c2f85fc8ee0613249a231a375e7bd8328b7
Felipe Pichl <felipe.pichl@hotmail.com>
2023-12-29 23:32:24 -0300

    RJ-2023: P04, M02 - Class 05 Part I

--- react-js/trail-2023/project-04/src/pages/index.tsx
+++ react-js/trail-2023/project-04/src/pages/index.tsx
@@ -1,10 +1,31 @@
-import { Inter } from 'next/font/google'
+import Image from "next/image"
 
+import { HomeContainer, Product } from "@/styles/pages/home"
 
-const inter = Inter({ subsets: ['latin'] })
+import shirt1 from '@/assets/camisetas/1.png'
+import shirt2 from '@/assets/camisetas/2.png'
+import shirt3 from '@/assets/camisetas/3.png'
 
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt=""/>
+      
+        <footer>
+          <strong>Shirt 1</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+      
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt=""/>
+      
+        <footer>
+          <strong>Shirt 2</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
