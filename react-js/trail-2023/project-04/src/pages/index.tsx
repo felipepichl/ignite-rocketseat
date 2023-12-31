54130a9dcc1358dfd38d929c7bb758a0586d4f3d
Felipe Pichl <felipe.pichl@hotmail.com>
2023-12-30 23:46:02 -0300

    RJ-2023: P04, M02 - Class 06

--- react-js/trail-2023/project-04/src/pages/index.tsx
+++ react-js/trail-2023/project-04/src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image"
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
 
 import { HomeContainer, Product } from "@/styles/pages/home"
 
@@ -7,9 +9,16 @@ import shirt2 from '@/assets/camisetas/2.png'
 import shirt3 from '@/assets/camisetas/3.png'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2.5,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={shirt1} width={520} height={480} alt=""/>
       
         <footer>
@@ -18,7 +27,7 @@ export default function Home() {
         </footer>
       </Product>
       
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt2} width={520} height={480} alt=""/>
       
         <footer>
@@ -26,6 +35,24 @@ export default function Home() {
           <span>R$ 79,99</span>
         </footer>
       </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt=""/>
+      
+        <footer>
+          <strong>Shirt 3</strong>
+          <span>R$ 149,99</span>
+        </footer>
+      </Product>
+
+      {/* <Product className="keen-sllider__slide">
+        <Image src={shirt3} width={520} height={480} alt=""/>
+      
+        <footer>
+          <strong>Shirt 3</strong>
+          <span>R$ 149,99</span>
+        </footer>
+      </Product> */}
     </HomeContainer>
   )
 }
