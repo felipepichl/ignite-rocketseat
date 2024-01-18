284b7248b24238f46eb16806d3699f344f3d6eba
Felipe Pichl <felipe.pichl@hotmail.com>
2024-01-17 23:20:16 -0300

    RJ-2023: P04, M04 - Class 02

--- react-js/trail-2023/project-04/src/pages/product/[id].tsx
+++ react-js/trail-2023/project-04/src/pages/product/[id].tsx
@@ -1,9 +1,30 @@
 import { useRouter } from 'next/router'
+import { 
+  ImageContainer, 
+  ProductContainer, 
+  ProductDetails 
+} from '@/styles/pages/products'
 
 export default function Product() {
   const { query } = useRouter()
   
   return (
-    <h1>Product</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste obcaecati quo rem similique fugiat aliquid repudiandae, minus id magni provident quas natus nam, minima, ab nisi rerum repellendus! Illo, qui!</p>
+
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
