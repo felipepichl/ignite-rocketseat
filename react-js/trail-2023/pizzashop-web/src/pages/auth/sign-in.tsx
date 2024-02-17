692bfcd6a9542f2447df55520050695dffa0ab88
Felipe Pichl <felipe.pichl@hotmail.com>
2024-02-16 23:15:01 -0300

    RJ-2023: PS, M02 - Class 01

--- react-js/trail-2023/pizzashop-web/src/pages/auth/sign-in.tsx
+++ react-js/trail-2023/pizzashop-web/src/pages/auth/sign-in.tsx
@@ -1,10 +1,34 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export default function SignIn() {
   return (
-    <div>
+    <>
       <Helmet title="Sign" />
-      <h1>SignIn</h1>
-    </div>
+      <div className="p-8">
+        <div className="w[350px] flex flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tighter">
+              Acessar Painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do prceiro!
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu Email</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button className="w-full">Acessa painel</Button>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
