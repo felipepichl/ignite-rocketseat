e6def8aeb99c34e094e3de59d11c398c1d0b8b7d
Felipe Pichl <felipe.pichl@hotmail.com>
2024-02-19 21:59:06 -0300

    RJ-2023: PS, M02 - Class 03

--- react-js/trail-2023/pizzashop-web/src/pages/auth/sign-in.tsx
+++ react-js/trail-2023/pizzashop-web/src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -23,6 +24,13 @@ export default function SignIn() {
     console.log(email)
 
     await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    toast.success('Enviamos um link de autenticação para o seu email', {
+      action: {
+        label: 'Reenviar',
+        onClick: () => handleSignIn({ email }),
+      },
+    })
   }
 
   return (
