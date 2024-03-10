e543a39cb6f90e22e465e4cd1ec649b9b16fa84a
Felipe Pichl <felipe.pichl@hotmail.com>
2024-03-09 22:42:50 -0300

    RJ-2023: PS, M02 - Class 10 Done

--- react-js/trail-2023/pizzashop-web/src/pages/app/orders/order-datails.tsx
+++ react-js/trail-2023/pizzashop-web/src/pages/app/orders/order-datails.tsx
@@ -4,7 +4,15 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
-import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 
 export function OrderDetails() {
   return (
@@ -20,7 +28,7 @@ export function OrderDetails() {
           <TableBody>
             <TableRow>
               <TableCell>Status</TableCell>
-              <TableCell>
+              <TableCell className="flex justify-end">
                 <div className="flex items-center gap-2">
                   <span className="h-2 w-2 rounded-full bg-slate-400" />
                   <span className="font-medium text-muted-foreground">
@@ -29,7 +37,58 @@ export function OrderDetails() {
                 </div>
               </TableCell>
             </TableRow>
+
+            <TableRow>
+              <TableCell>Cliente</TableCell>
+              <TableCell className="flex justify-end">Felipe Pichl</TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell>Telefone</TableCell>
+              <TableCell className="flex justify-end">555599009999</TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell>E-mail</TableCell>
+              <TableCell className="flex justify-end">
+                felipe.pichl@example.com
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell>Realizado há</TableCell>
+              <TableCell className="flex justify-end">há 3 minutos</TableCell>
+            </TableRow>
           </TableBody>
+        </Table>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Produto</TableHead>
+              <TableHead className="text-right">Qtd.</TableHead>
+              <TableHead className="text-right">Preço</TableHead>
+              <TableHead className="text-right">Subtotal</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>Pizza pepperoni Família</TableCell>
+              <TableCell className="text-right">2</TableCell>
+              <TableCell className="text-right">R$ 50,00</TableCell>
+              <TableCell className="text-right">R$ 100,00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Pizza mussarela Família</TableCell>
+              <TableCell className="text-right">2</TableCell>
+              <TableCell className="text-right">R$ 39,00</TableCell>
+              <TableCell className="text-right">R$ 78,00</TableCell>
+            </TableRow>
+          </TableBody>
+          <TableFooter>
+            <TableCell colSpan={3}>Total</TableCell>
+            <TableCell className="text-right font-medium">R$ 109,00</TableCell>
+          </TableFooter>
         </Table>
       </div>
     </DialogContent>
