1701f24b7ea8730624e03eb2d1c9182f6db0fe53
Felipe Pichl <felipe.pichl@hotmail.com>
2024-03-13 20:15:34 -0300

    RJ-2023: PS, M02 - Class 11 Done

--- react-js/trail-2023/pizzashop-web/src/pages/app/dashboard/dashboard.tsx
+++ react-js/trail-2023/pizzashop-web/src/pages/app/dashboard/dashboard.tsx
@@ -1,7 +1,9 @@
-import { DollarSign } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { DayOrdersAmountCard } from './day-orders-amount-card'
+import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount'
+import { MonthOrdersAmountCard } from './month-orders-amount-card'
+import { MonthRevenueCard } from './month-revenue-card'
 
 export function Dashboard() {
   return (
@@ -11,25 +13,10 @@ export function Dashboard() {
         <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
 
         <div className="grid grid-cols-4 gap-4">
-          <Card>
-            <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-base font-semibold">
-                Receita total (mês)
-              </CardTitle>
-              <DollarSign className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent className="space-y-1">
-              <span className="text-2xl font-bold tracking-tighter">
-                R$ 1.248,68
-              </span>
-              <p className="text-xs text-muted-foreground">
-                <span className="text-emerald-500 dark:text-emerald-400">
-                  +2%
-                </span>{' '}
-                em relação ao mês passado
-              </p>
-            </CardContent>
-          </Card>
+          <MonthRevenueCard />
+          <MonthOrdersAmountCard />
+          <DayOrdersAmountCard />
+          <MonthCanceledOrdersAmountCard />
         </div>
       </div>
     </div>
